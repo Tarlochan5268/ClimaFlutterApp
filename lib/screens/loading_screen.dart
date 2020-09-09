57178a8c2cbf8cd13ce8bb71a44f42ea8168df0a
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-09-09 16:32:00 -0400

    Clima Completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,42 +1,42 @@
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as HTTP;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-import '../services/location.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> getLocation() async {
-    location loc = new location();
-    await loc.getCurrentLocation();
-    print(loc.latitude);
-    print(loc.longitude);
-  }
-
-  void getData() async {
-    HTTP.Response response = await HTTP.get(
-        'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02');
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
-    } else {
-      print(response.statusCode);
-    }
-  }
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getData();
-    getLocation();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,13 +1,13 @@
 import 'package:geolocator/geolocator.dart';
 
-class location {
+class Location {
   double latitude;
   double longitude;
 
   Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
