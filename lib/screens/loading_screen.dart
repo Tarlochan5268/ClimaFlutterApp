8c2303916f27aaeb50fb5fa9d8f013589e5ee7a4
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-09-09 10:04:03 -0400

    Async wait Http and Json Decode from API completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as HTTP;
 
 import '../services/location.dart';
 
@@ -15,10 +16,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(loc.longitude);
   }
 
+  void getData() async {
+    HTTP.Response response = await HTTP.get(
+        'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getData();
     getLocation();
   }
 
